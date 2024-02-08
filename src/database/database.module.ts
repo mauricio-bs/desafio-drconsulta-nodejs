a4027ae4dd41afb8aeb77fecb86d1f16b158b683
mauricio-bs <mauricio.3g2014@gmail.com>
2024-02-08 13:49:57 -0300

    feat: Add dedicated module for database configurations

---
+++ src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST') || 'localhost',
+        port: configService.get<number>('DB_PORT') || 3306,
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: true,
+        logging: true,
+        autoLoadEntities: true,
+        entities: [__dirname, '/../entities/*.entity{.ts,.js}'],
+        migrations: [__dirname, '/migrations/*{.ts,.js}'],
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
