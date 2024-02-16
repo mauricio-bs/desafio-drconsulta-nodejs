832638f92a35e089356a10b388e743d93fcf8598
mauricio-bs <mauricio.3g2014@gmail.com>
2024-02-16 18:26:39 -0300

    feat: update general module imports

--- src/modules/general.module.ts
+++ src/modules/general.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from './auth/auth.module';
 import { CompanyModule } from './company/company.module';
+import { ParkingModule } from './parking/parking.module';
+import { UserModule } from './user/user.module';
+import { VehicleModule } from './vehicle/vehicle.module';
 
 @Module({
-  imports: [CompanyModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    CompanyModule,
+    VehicleModule,
+    ParkingModule,
+  ],
 })
 export class GeneralModule {}
